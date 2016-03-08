9cf932a173c9379978b5bb5f934cbe001be037f7
Kevin Tyler <kevin.patrick.tyler@gmail.com>
2016-03-07 21:44:51 -0500

    added JTS implementation of convex hull

--- src/test/java/ConvexHullTester.java
+++ src/test/java/ConvexHullTester.java
@@ -1,5 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -12,6 +15,10 @@ import java.util.Random;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import com.vividsolutions.jts.algorithm.ConvexHull;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
 import usace.army.mil.erdc.Pivots.Utilities.PivotUtilities;
 import usace.army.mil.erdc.pivots.models.IPoint;
 import usace.army.mil.erdc.pivots.models.Point;
@@ -21,10 +28,27 @@ import usace.army.mil.erdc.pivots.models.PointFactory;
 public class ConvexHullTester {
 	private final static String CALIFORNIA_ROADS_PATH = "/home/ktyler/Documents/strider/CaliforniaRoadNetworksNodes.txt";
 	private final static String WALKING_DEAD_TWEETS_PATH = "/home/ktyler/Documents/misc/twitter_sm.tsv";
-	private static Point selectPointFromListRandomly(List<Point> points){
-		Random random = new Random();
-		return points.get(random.nextInt(10000));
-	}
+	private static void writePointsToWRT(List<Point> points, String filename){
+		try{
+		File file = new File(filename);
+
+		// if file doesn't exists, then create it
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		for(Point point : points){
+			bw.write("\"Point (" + point.getX() + " " + point.getY() + ")\"\n");
+		}
+		
+		bw.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+
+}
 	
 	 private static List<Point> populatePointsFromWalkingDeadDataset(){
 			PointFactory pointFactory = new PointFactory();
@@ -58,49 +82,94 @@ public class ConvexHullTester {
 		return points;
 	}
 	
-	private static Set<IPoint> getUniquePoints(IPoint [] points){
+	private static List<Point> getUniquePoints(IPoint [] points){
 		Set<IPoint> uniquePoints = new HashSet<IPoint>();
 		List<IPoint> duplicatePoints = new ArrayList<IPoint>();
+		List<Point> pointList = new ArrayList<Point>();
 		for(int i = 0; i < points.length; i++){
 			if(uniquePoints.contains(points[i])){
 				duplicatePoints.add(points[i]);
 			} else{
-				uniquePoints.add(points[i]);
+					if(i != points.length -1){
+						uniquePoints.add(points[i]);
+						pointList.add((Point)points[i]);
+					}
 			}
 		}
-		return uniquePoints;
+		return pointList;
+	}
+	
+	private static void runOReillyConvexHullTest(List<Point> californiaPoints, List<Point> zombiePoints){
+		//Test California Roads Dataset
+				
+				Point [] caliPoints = new Point[californiaPoints.size()];
+				caliPoints = californiaPoints.toArray(caliPoints);
+				writePointsToWRT(californiaPoints, "/tmp/californiaPoints.csv");
+				
+				
+				IPoint [] convexHull = PivotUtilities.compute(caliPoints);
+				List<Point> uniqueHullPoints = getUniquePoints(convexHull);
+				writePointsToWRT(uniqueHullPoints, "/tmp/californiaHullPoints.csv");
+				
+				assert(convexHull.length < caliPoints.length);
+				System.out.println("Original dataset size: " + caliPoints.length);
+				System.out.println("Convex hull size: " + convexHull.length);
+				System.out.println("Unique hull size: " + uniqueHullPoints.size());
+				
+				//Test Walking Dead Twitter Dataset
+				
+				writePointsToWRT(zombiePoints, "/tmp/zombiePoints.csv");
+				Point [] zombiePointsArray = new Point[californiaPoints.size()];
+				zombiePointsArray = zombiePoints.toArray(zombiePointsArray);
+				
+				IPoint [] zombieHull = PivotUtilities.compute(zombiePointsArray);
+				List<Point> uniqueZombieHullPoints = getUniquePoints(zombieHull);
+				
+				writePointsToWRT(uniqueZombieHullPoints, "/tmp/zombieHullPoints.csv");
+				
+				assert(zombieHull.length < zombiePointsArray.length);
+				if(zombieHull.length < zombiePointsArray.length){
+					System.out.println("Successful convex hull computation achieved");
+				} else{
+					System.out.println("Convex hull formed erroneously.");
+				}
+				System.out.println("Original dataset size: " + zombiePointsArray.length);
+				System.out.println("Convex hull size: " + zombieHull.length);
+				System.out.println("Unique hull size: " + uniqueZombieHullPoints.size());
+				//1,132,878
+				//1,238,920
+	}
+	
+	private static Coordinate [] convertPointListToCoordArray(List<Point> points){
+		Coordinate [] coordinates = new Coordinate[points.size()];
+		int i = 0;
+		for(Point point : points){
+			coordinates[i] = new Coordinate(point.getX(), point.getY());
+			i++;
+		}
+		return coordinates;
+	}
+	
+	private static List<Point> convertCoordArrayToPointList(Coordinate [] coordinates){
+		List<Point> pointList = new ArrayList<Point>();
+		for(int i = 0; i < coordinates.length; i++){
+			pointList.add(new Point(coordinates[i].x, coordinates[i].y));
+		}
+		return pointList;
+	}
+	
+	private static void runJTSConvexHullTest(List<Point> californiaPoints, List<Point> zombiePoints){
+		ConvexHull convexHull = new ConvexHull(convertPointListToCoordArray(californiaPoints), new GeometryFactory());
+		writePointsToWRT(convertCoordArrayToPointList(convexHull.getConvexHull().getCoordinates()), "/tmp/californiaHullPoints_JTS.csv");
+		
+		ConvexHull zombieConvexHull = new ConvexHull(convertPointListToCoordArray(zombiePoints), new GeometryFactory());
+		writePointsToWRT(convertCoordArrayToPointList(zombieConvexHull.getConvexHull().getCoordinates()), "/tmp/zombieHullPoints_JTS.csv");
 	}
 	
 	public static void main(String [] args){
-		//Test California Roads Dataset
 		List<Point> californiaPoints = populatePointsFromCaliforniaRoadsDataset();
-		Point [] caliPoints = new Point[californiaPoints.size()];
-		caliPoints = californiaPoints.toArray(caliPoints);
-		
-		IPoint [] convexHull = PivotUtilities.compute(caliPoints);
-		Set<IPoint> uniqueHullPoints = getUniquePoints(convexHull);
-		assert(convexHull.length < caliPoints.length);
-		System.out.println("Original dataset size: " + caliPoints.length);
-		System.out.println("Convex hull size: " + convexHull.length);
-		System.out.println("Unique hull size: " + uniqueHullPoints.size());
-		
-		//Test Walking Dead Twitter Dataset
 		List<Point> zombiePoints = populatePointsFromWalkingDeadDataset();
-		Point [] zombiePointsArray = new Point[californiaPoints.size()];
-		zombiePointsArray = zombiePoints.toArray(zombiePointsArray);
-		
-		IPoint [] zombieHull = PivotUtilities.compute(zombiePointsArray);
-		Set<IPoint> uniqueZombieHullPoints = getUniquePoints(zombieHull);
-		assert(zombieHull.length < zombiePointsArray.length);
-		if(zombieHull.length < zombiePointsArray.length){
-			System.out.println("Successful convex hull computation achieved");
-		} else{
-			System.out.println("Convex hull formed erroneously.");
-		}
-		System.out.println("Original dataset size: " + zombiePointsArray.length);
-		System.out.println("Convex hull size: " + zombieHull.length);
-		System.out.println("Unique hull size: " + uniqueZombieHullPoints.size());
-		//1,132,878
-		//1,238,920
+		//runOReillyConvexHullTest(californiaPoints, zombiePoints);
+		runJTSConvexHullTest(californiaPoints, zombiePoints);
 	}
 }
